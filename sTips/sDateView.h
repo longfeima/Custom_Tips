0ed9f7bb566739bdc3116edc2c8743ed25917192
Seven <Seven@Seven-3.local>
2017-08-14 13:55:31 +0800

    新增DateView自定制日期View

---
+++ sTips/sDateView.h
@@ -0,0 +1,13 @@
+//
+//  sDateView.h
+//  sTips
+//
+//  Created by Seven on 2017/8/14.
+//  Copyright © 2017年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sDateView : UIView
+
+@end
---
+++ sTips/sDateView.m
@@ -0,0 +1,21 @@
+//
+//  sDateView.m
+//  sTips
+//
+//  Created by Seven on 2017/8/14.
+//  Copyright © 2017年 Seven. All rights reserved.
+//
+
+#import "sDateView.h"
+
+@implementation sDateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
